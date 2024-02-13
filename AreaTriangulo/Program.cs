9b3617f3410945ccf669e4e8dd36ccec7aa7e233
abrah <abrah@HPDEABRAHAM>
2024-02-12 21:25:02 -0600

    Agregar archivos de proyecto.

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double b, h, a;
+Console.WriteLine("Enter the height:");
+h = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the base:");
+b = double.Parse(Console.ReadLine());
+a = (b * h) / 2;
+Console.WriteLine("The area of the triangle is:" + a);
+Console.ReadKey();
